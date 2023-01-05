6b38c0aa5ca1159d3d80b00a6b1ae51bda458a4d
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-05 09:42:25 +0100

    Add some dom manipulation

---
+++ dom-intro/index.js
@@ -0,0 +1,10 @@
+
+let theCatDiv = document.getElementById("cat");
+
+theCatDiv.innerHTML = "I'm a cat!"
+
+theCatDiv.style.backgroundColor = "red";
+theCatDiv.style.border = "2px solid black";
+theCatDiv.style.fontSize ="50px";
+theCatDiv.style.marginTop ="30px";
+theCatDiv.style.paddingBottom ="30px";
