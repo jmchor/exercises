9b342b9bf68bffb9b2526942bf9c47623266712b
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-07 17:01:06 +0100

    Add more dom ex

--- dom-intro/index.js
+++ dom-intro/index.js
@@ -1,10 +1,40 @@
+let googleLink = document.getElementById('google-link');
 
-let theCatDiv = document.getElementById("cat");
+googleLink.setAttribute('href', 'https://www.google.com');
+googleLink.setAttribute('target', '_blank');
+let body = document.querySelector('body');
+let para = document.createElement('p');
+let ul = document.getElementById('items-list');
 
-theCatDiv.innerHTML = "I'm a cat!"
 
-theCatDiv.style.backgroundColor = "red";
-theCatDiv.style.border = "2px solid black";
-theCatDiv.style.fontSize ="50px";
-theCatDiv.style.marginTop ="30px";
-theCatDiv.style.paddingBottom ="30px";
+let button = document.getElementById('add-item-button');
+let i = 5;
+button.onclick = function() {
+
+    i++;
+
+    let li = document.createElement('li');
+    let text = document.createTextNode(`Item number ${i}`);
+
+    li.appendChild(text);
+    ul.appendChild(li);
+    }
+
+let input = document.getElementById('input-field');
+
+    let liTags = document.getElementsByTagName('li');
+
+    // for (let i = 0; i < liTags.length; i++) {
+    //     liTags[i].onclick = function(e) {
+    //     console.log(e.currentTarget.innerText   );
+    //     };
+    // }
+
+    function deleteContent(e) {
+        return e.currentTarget.remove();
+    }
+
+    let ulList = document.querySelectorAll('.list li');
+ulList.forEach((li)=> {
+    li.addEventListener('click', deleteContent);
+})
