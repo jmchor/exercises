8f2c0af0d695e60405ccc4aada71da83ea748f85
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-08 15:00:24 +0100

    Add round functionality

--- simon-says-unresolved/main.js
+++ simon-says-unresolved/main.js
@@ -2,6 +2,7 @@ let domSquareOne = document.getElementById("square-one")
 let domSquareTwo = document.getElementById("square-two")
 let domSquareThree = document.getElementById("square-three")
 let domSquareFour = document.getElementById("square-four")
+let numberOfRounds = document.getElementById("round-input").value
 
 computerNumbers = []
 playerNumbers = []
@@ -18,25 +19,25 @@ function highlight(color) {
         case 1:
             domSquareOne.style.backgroundColor = "red"
             setTimeout( () => {
-                domSquareOne.style.backgroundColor = "darkred"
+                domSquareOne.style.backgroundColor = "#580606"
             }, 200)
             break;
         case 2:
             domSquareTwo.style.backgroundColor = "green"
             setTimeout( () => {
-                domSquareTwo.style.backgroundColor = "darkgreen"
+                domSquareTwo.style.backgroundColor = "#023b02"
             }, 200)
             break;
         case 3:
             domSquareThree.style.backgroundColor = "blue"
             setTimeout( () => {
-                domSquareThree.style.backgroundColor = "darkblue"
+                domSquareThree.style.backgroundColor = "#09095c"
             }, 200)
             break;
         case 4:
             domSquareFour.style.backgroundColor = "yellow"
             setTimeout( () => {
-                domSquareFour.style.backgroundColor = "goldenrod"
+                domSquareFour.style.backgroundColor = "#7a601c"
             }, 200)
             break;
 
@@ -51,17 +52,19 @@ let doComputerPlays = () => {
     console.log(computerNumbers)
 
     computerNumbers.forEach((number, index) => {
-
         setTimeout(() => {
-
             highlight(number)
+        }, index * 500 + 500)
 
-
-        }, index * 1000)
+    if (index === computerNumbers.length - 1)
+        {
+            // after all the colors got played, set
+            setTimeout(() => {
+                isPlayerTurn = true
+            }, index * 500 + 500)
+        }
 
     })
-
-
 
 }
 
@@ -76,33 +79,30 @@ function playerClick(numberClicked){
 
     if (!isPlayerTurn) return
 
-
     highlight(numberClicked)
 
     playerNumbers.push(numberClicked)
+    console.log(playerNumbers)
     let areArraysEqual = checkIfArraysAreEqual()
 
     if (areArraysEqual) {
-
         // highlight only if the arrays are equal !!!
         highlight(numberClicked)
-
         // check for last item of the arrays
-        if (playerNumbers.length == computerNumbers.length)
+        if ( playerNumbers.length === Number(numberOfRounds)) {
+
+            alert("you won!!!")
+    } else if (playerNumbers.length == computerNumbers.length)
         {
             isPlayerTurn = false
             computerTurn()
-
             // empty array please !!
             playerNumbers = []
         }
-    }
-    else
-    {
-        alert("game over!!")
-    }
 
-}
+}   else    {
+    alert("game over!!")
+}}
 
 let checkIfArraysAreEqual = () => {
     slicedNumbers = computerNumbers.slice(0, playerNumbers.length)
@@ -123,3 +123,4 @@ let checkIfArraysAreEqual = () => {
 function getRandomNumber(){
     return Math.floor(Math.random() * 4) + 1
 }getRandomNumber()
+
