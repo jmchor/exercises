df3ac57627edc72c0739da9363ff58efdb729dd9
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-02 14:35:57 +0100

    Add final structure

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response, next) => response.sendFile(`${__dirname}/views/home-page.html`));
+
+// ...
+
+app.get('/cat', (request, response, next) => response.sendFile(`${__dirname}/views/cat-page.html`));
+
+// ...
+
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
