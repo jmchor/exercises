ead63997cc8d0bd2c4e37e5fae8d5b43cd1b3028
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-02 13:17:41 +0100

    edit eslintrc

--- node-http-server/server.js
+++ node-http-server/server.js
@@ -1,18 +1,18 @@
-const http = require('http')
-const server = http.createServer((request, response)=> {
+const http = require('http');
 
-    console.log(`Someone has requested ${request.url}`)
+const server = http.createServer((request, response) => {
+        console.log(`Someone has requested ${request.url}`);
 
-    if(request.url === '/') {
-        response.write('Hello, world!');
-        response.end()
-    } else if (request.url === '/about') {
-        response.write('My name is Izzy');
-        response.end();
-      }else {
-        response.statusCode = 404;
-        response.write('404 Page');
-        response.end()
-    }
+        if (request.url === '/') {
+                response.write('Hello, world!');
+                response.end();
+        } else if (request.url === '/about') {
+                response.write('My name is Izzy');
+                response.end();
+        } else {
+                response.statusCode = 404;
+                response.write('404 Page');
+                response.end();
+        }
 });
-server.listen(3000)
+server.listen(3000);
