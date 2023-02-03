8fc128c3a0a5e61f6365b4da23c7d69de014bbac
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-03 14:50:16 +0100

    edit app.js

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -13,3 +13,15 @@ app.get('/cat', (request, response, next) => response.sendFile(`${__dirname}/vie
 // ...
 
 app.listen(3000, () => console.log('My first app listening on port 3000! '));
+
+// if ES Modules should be used, it'S this :
+
+// import express from 'express';
+
+// because ESM doesn't have filename or dirname we need to import this
+// import * as url from 'url';
+
+// const __filename = url.fileURLToPath(import.meta.url);
+// const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+// change file extension to .mjs or set type to module in package.json
