7c9db21946b7c95bea0477b8a2c8e068a0151944
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-10 10:31:51 +0100

    Add submitting & accessing req.query

--- express-get-params/app.js
+++ express-get-params/app.js
@@ -30,4 +30,14 @@ app.get('/get-user-info', (req, res) => {
         res.render('user-info-form');
 });
 
+// the form on get-user-info is a GET req by default; the form has an action named the same as this app.get route, hence it will display the submitted info access through req.query
+app.get('/display-user-info', (req, res) => {
+        const { name, age, superhero } = req.query;
+        res.send(`
+    Your name is ${name}
+    Your age is ${age}
+    Your favorite superhero is ${superhero}
+  `);
+});
+
 app.listen(3000, () => console.log('App listening on port 3000!'));
