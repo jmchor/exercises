7e974276a262e6144423f186ea4e8b2fefafbff0
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-22 16:48:29 +0100

    Add bcrypt

---
+++ bcryptdemo/exampleWithPromises.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs');
+
+const saltRounds = 10;
+
+const plainPassword1 = 'HelloWorld';
+
+bcrypt.genSalt(saltRounds)
+        .then((salt) => {
+                console.log(`Salt: ${salt}`);
+
+                return bcrypt.hash(plainPassword1, salt);
+        })
+        .then((hash) => console.log(`Hash: ${hash}`))
+        .catch((err) => console.error(err.message));
---
+++ bcryptdemo/timeTest.js
@@ -0,0 +1,11 @@
+// bcryptDemo/timeTest.js
+
+const bcrypt = require('bcryptjs');
+
+const plainPassword1 = 'HelloWorld';
+
+for (let saltRounds = 10; saltRounds < 19; saltRounds += 1) {
+        console.time(`bcrypt | cost: ${saltRounds}, time to hash`);
+        bcrypt.hashSync(plainPassword1, saltRounds);
+        console.timeEnd(`bcrypt | cost: ${saltRounds}, time to hash`);
+}
