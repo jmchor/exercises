40a14b41e99639b8f8acd25604bf7a8fc51989a0
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-24 16:31:04 +0100

    Add user login and signup forms

--- basic-auth/routes/auth.routes.js
+++ basic-auth/routes/auth.routes.js
@@ -63,4 +63,34 @@ router.post('/signup', (req, res, next) => {
 
 router.get('/userProfile', (req, res) => res.render('users/user-profile'));
 
+/* ---------------------------------------LOGIN--------------------------------------- */
+
+router.get('/login', (req, res) => res.render('auth/login'));
+
+router.post('/login', async (req, res, next) => {
+        const { email, password } = req.body;
+
+        if (email === '' || password === '') {
+                res.render('auth/login', {
+                        errorMessage: 'Please enter both email and password to log in.',
+                });
+                return;
+        }
+
+        try {
+                const user = await User.findOne({ email });
+                if (!user) {
+                        res.render('auth/login', {
+                                errorMessage: 'Email is not registered. Try with other email.',
+                        });
+                } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+                        res.render('users/user-profile', { user });
+                } else {
+                        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+                }
+        } catch (error) {
+                next(error);
+        }
+});
+
 module.exports = router;
