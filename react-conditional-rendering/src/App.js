1365c1f04ee6aaa87ce2050fd6768df23038c6bf
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-31 19:27:13 +0200

    Add ex on conditional rendering

---
+++ react-conditional-rendering/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import MovieList from './components/MovieList';
+import Spinner from "./components/Spinner"
+import { useState } from 'react';
+import ImprovedMovieList from './components/ImprovedMovieList';
+
+
+function App() {
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  if(isLoading) {
+    return <Spinner />
+  } else {
+    return (
+      <div className="App">
+        {/* <MovieList /> */}
+        <ImprovedMovieList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-conditional-rendering/src/components/MovieCard.js
@@ -0,0 +1,22 @@
+function MovieCard(props) {
+	const { movie, deleteMovie } = props;
+
+	return (
+		<div className='MovieCard'>
+			<h3>{movie.title}</h3>
+			<p>Director: {movie.director}</p>
+			<p>Rating: {movie.IMDBRating}</p>
+
+			{/* {movie.hasOscars && <p>Got the Oscar Award! </p>}
+			{!movie.hasOscars && <p>Great movie but no Oscars! </p>}
+			 */}
+
+			{movie.hasOscars ? <p>Got the Oscar Award! </p> : <p>Great movie but no Oscars! </p> }
+			<button onClick={() => deleteMovie(movie._id)} className='btn-delete'>
+				Delete
+			</button>
+		</div>
+	);
+}
+
+export default MovieCard;
