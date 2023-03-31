d499109ea66f585bdecff1711d5f09537a41ee11
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-31 19:27:29 +0200

    Add react lists

---
+++ react-lists/src/App.js
@@ -0,0 +1,22 @@
+// App.js
+
+import './App.css';
+import SimpleList from './components/SimpleList';
+import StudentList from "./components/StudentList"
+import ProjectList from './components/ProjectsList';
+import MovieList from './components/MovieList';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <List /> */}
+      {/* <StudentList /> */}
+      {/* <ProjectList /> */}
+
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
+
