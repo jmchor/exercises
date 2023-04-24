1cb9fca5add6776a1c263c28ed450790ab21aacc
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-24 07:25:45 +0200

    add context wrapper

--- project-management-client/src/main.jsx
+++ project-management-client/src/main.jsx
@@ -5,13 +5,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { AuthProviderWrapper } from './context/auth.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
         <React.StrictMode>
                 <Router>
-                        <App />
+                        <AuthProviderWrapper>
+                                <App />
+                        </AuthProviderWrapper>
                 </Router>
         </React.StrictMode>
 );
